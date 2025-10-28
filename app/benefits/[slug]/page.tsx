d818ac54e9eecae383380bc98b3d058020ed9e13
HumphreyCurtis <humphreywcurtis@gmail.com>
2025-10-28 11:35:26 +0000

    Added working code to retrieve data and query by slug

--- app/benefits/[slug]/page.tsx
+++ app/benefits/[slug]/page.tsx
@@ -1,12 +1,27 @@
 import { use } from "react";
 import { notFound } from "next/navigation";
 import benefitData from "@/data/home.json";
+import type { Benefit } from "@/app/types";
 
-type Item = { slug: string; title: string }; // Rewrite by forming proper external Types file
+// type Item = { slug: string; title: string }; // Rewrite by forming proper external Types file
 
-export async function generateStaticParams() {
-  const items = (await import("@/data/home.json")).default as Item[];
-  return items.map((b) => ({ slug: b.slug }));
+// export async function generateStaticParams() {
+//   const items = (await import("@/data/home.json")).default as Item[];
+//   return items.map((b) => ({ slug: b.slug }));
+// }
+
+export function getAllBenefits(): Benefit[] {
+  return benefitData;
+}
+
+// Move to library
+export function getBenefitDataBySlug(slug: string): Benefit {
+  const benefit = benefitData.find((b) => b.slug === slug);
+  if (!benefit) {
+    throw new Error(`Benefit with slug not found`);
+  }
+  console.log(benefit);
+  return benefit;
 }
 
 // Taken from NextJS course -- rewrite the page call
@@ -20,13 +35,16 @@ export async function generateStaticParams() {
 //   return foundModel
 // }
 
-export default function BenefitPage({
+export default async function BenefitPage({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const { slug } = use(params); // unwrap Promise
-  const pageBenefit = (benefitData as Item[]).find((b) => b.slug === slug);
-  if (!pageBenefit) notFound();
-  return <h1>{pageBenefit.title}</h1>;
+  const { slug } = await params;
+  const benefit = getBenefitDataBySlug(slug);
+
+  // const { slug } = use(params); // unwrap Promise
+  // const pageBenefit = (benefitData as Item[]).find((b) => b.slug === slug);
+  // if (!pageBenefit) notFound();
+  return <h1>{benefit.title}</h1>;
 }
